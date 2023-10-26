d1aa4d5c9856c1bab5eda7a3585a28e5543e1ed1
HeitorBarbosaDaRocha <134523231+HeitorBarbosaDaRocha@users.noreply.github.com>
2023-10-26 12:08:40 -0300

    aaaa som

--- main.js
+++ main.js
@@ -35,12 +35,12 @@ function tocaSomPsh(){
 document.querySelector('.tecla_psh').onclick = tocaSomPsh
 
 function tocaSomTic(){
-    document.querySelector('#som_tecla_Tic').play();
+    document.querySelector('#som_tecla_tic').play();
 }
-document.querySelector('.tecla_Tic').onclick = tocaSomTic
+document.querySelector('.tecla_tic').onclick = tocaSomTic
 
 function tocaSomTom(){
-    document.querySelector('#som_tecla_Tom').play();
+    document.querySelector('#som_tecla_tom').play();
 }
 document.querySelector('.tecla_tom').onclick = tocaSomTom
 
